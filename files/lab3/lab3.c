251a33d980c644f8752e238c16131e5e4c5618fd
ArtemBoxberger <coder1712@gmail.com>
2023-05-01 17:44:44 +0700

    add maps reading

--- files/lab3/lab3.c
+++ files/lab3/lab3.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
-#include <string.h>
 #include <fcntl.h>
 #include <unistd.h>
 
@@ -14,30 +13,52 @@ int main(int argc, char *argv[]) {
         exit(1);
     }
 
+    char maps_path [256];
     char pagemap_path[256];
     sprintf(pagemap_path, "/proc/%s/pagemap", argv[1]);
+    sprintf(maps_path, "/proc/%s/maps", argv[1]);
 
-    int fd = open(pagemap_path, O_RDONLY);
-    if (fd == -1) {
+
+    int pagemap_fd = open(pagemap_path, O_RDONLY);
+    if (pagemap_fd == -1) {
         perror("open err");
         exit(1);
     }
 
-    uint64_t pagemap_entry;
-    ssize_t bytes_read;
+    FILE *maps_file = fopen(maps_path, "r");
+    if (!maps_file) {
+        perror("fopen err");
+        exit(1);
+    }
+
+    uint64_t start_addr;
+    uint64_t end_addr;
+    uint64_t offset;
     uint64_t page_frame_number;
     uint64_t page_flags;
+    char buf[256];
 
-    while ((bytes_read = read(fd, &pagemap_entry, PAGEMAP_LENGTH)) == PAGEMAP_LENGTH) {
-        page_frame_number = pagemap_entry & 0x7fffffffffffff;
-        page_flags = pagemap_entry >> 55;
+    while (fgets(buf, sizeof(buf), maps_file)) {
+        sscanf(buf, "%lx - %lx", &start_addr, &end_addr);
+        offset = start_addr / PAGE_SIZE * PAGEMAP_LENGTH;
+
+        if (lseek(pagemap_fd, offset, SEEK_SET) == -1) {
+            perror("lseek err");
+            exit(1);
+        }
+
+        if (read(pagemap_fd, &page_frame_number, PAGEMAP_LENGTH) != PAGEMAP_LENGTH) {
+            perror("read pagemap err");
+            exit(1);
+        }
+
+        page_frame_number = page_frame_number & 0x7fffffffffffff;
+        page_flags = page_frame_number >> 55;
+//        printf("start-end addr: 0x%lx - 0x%lx, // ", start_addr, end_addr );
         printf("frame num: %lu flags: %lu\n", page_frame_number, page_flags);
     }
 
-    if (bytes_read == -1) {
-        perror("read err");
-        exit(1);
-    }
-    close(fd);
+    close(pagemap_fd);
+    fclose(maps_file);
     return 0;
 }
